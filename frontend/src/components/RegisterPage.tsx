66ca7a459e24cb1be731041d5690346f66f38f08
Erik Busch <erik_busch@gmx.de>
2022-07-05 16:04:14 +0200

    resloved errors

--- frontend/src/components/RegisterPage.tsx
+++ frontend/src/components/RegisterPage.tsx
@@ -1,4 +1,4 @@
-import {FormEvent, useEffect, useState} from "react";
+import {FormEvent,useState} from "react";
 import {useNavigate} from "react-router-dom";
 import {registerNow} from "./apiService/apiServices";
 
